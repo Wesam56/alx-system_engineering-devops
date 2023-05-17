3e70d601590612b47ea213788fbce7732cb65174
Wesam56 <113597907+Wesam56@users.noreply.github.com>
2023-05-17 04:12:43 +0400

    Delete 0x14-mysql directory

--- 0x14-mysql/making_of_this_project/1-create_user.sql
+++
@@ -1,3 +0,0 @@
-sudo mysql
-CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* to 'holberton_user'@'localhost';
--- 0x14-mysql/making_of_this_project/3-replica_user.sql
+++
@@ -1,4 +0,0 @@
-sudo mysql
-CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'replica_user';
-GRANT REPLICATION SLAVE ON *.* to 'replica_user'@'%';
-GRANT SELECT ON TABLE mysql.user TO 'holberton_user'@'localhost';
