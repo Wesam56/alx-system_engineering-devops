66d09de5e3f9d35456c954e3c3e2ab099f58eb7c
Wesam56 <113597907+Wesam56@users.noreply.github.com>
2023-09-14 19:41:35 +0300

    Add files via upload

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# puppet file to automate a 500 error fix
-exec { 'fixed-phpp':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin';
+# automated puppet fix (to find out why Apache is returning a 500 error)
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
